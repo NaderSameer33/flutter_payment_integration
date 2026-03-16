da49d605db3e0614d39977740d02a88a256c22da
NaderSameer <nadersameer@std.mans.edu.eg>
2026-03-16 22:11:06 +0200

    build a strture for build cartView

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:payment_integration/features/checkout/presentation/views/my_cart_view.dart';
 
 void main() {
-  runApp(const PaymentInegration());
+  runApp(const ChcekOutApp());
 }
 
-class PaymentInegration extends StatelessWidget {
-  const PaymentInegration({super.key});
+class ChcekOutApp extends StatelessWidget {
+  const ChcekOutApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Payment Integration'),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
       ),
+      home: MyCartView(),
     );
   }
 }
